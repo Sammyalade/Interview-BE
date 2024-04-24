e02814e63c969b7ffd99d475977a25a4a6800c79
tochi27 <tochukwuamaechina2703@gmail.com>
2024-04-24 16:09:02 +0100

    Update nodemailer

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,32 +1,32 @@
-const nodemailer = require("nodemailer")
+const nodemailer = require("nodemailer");
 
-
-//Create Email Transporter
-const sendEmail = async(subject, message, send_to, sent_from, reply_to) =>{
-    const transporter = nodemailer.createTransport ({
-        service: 'gmail',
+// Create Email Transporter
+const sendEmail = async (subject, message, send_to, sent_from, reply_to) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
     auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS, // Use an App Password for enhanced security
+      user: "developer@awarri.com",
+      pass: 'jevn xdrl egpc vhyn', // Use an App Password for enhanced security
     },
+    from: "developer@awarri.com",
+  });
 
-    })
-    // Options for sending Email
-    const options = {
-        from: sent_from,
-        to: send_to,
-        replyTo: reply_to,
-        subject:  subject,
-        html: message,
-    }
-        //Send the email
-        transporter.sendMail(options, function (err, info){
-            if(err){
-                console.log(err)
-            }else {
-            console.log(info);
-            }
-        })
+  // Options for sending Email
+  const options = {
+    from: sent_from,
+    to: send_to,
+    replyTo: reply_to,
+    subject: subject,
+    html: message,
+  };
+
+  try {
+    // Send the email
+    const info = await transporter.sendMail(options);
+    console.log("Email sent:", info);
+  } catch (error) {
+    console.error("Error sending email:", error);
+  }
 };
 
-module.exports = sendEmail
+module.exports = sendEmail;

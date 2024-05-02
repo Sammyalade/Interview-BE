2f9ee119d71771585a2f0b45dc685dd7bb8f175e
excelthedev <tobyadeyy@gmail.com>
2024-05-02 14:56:04 +0100

    fix: pushing record to gcp

---
+++ utils/frontEndUrl.js
@@ -0,0 +1,6 @@
+const frontEndUrl = {
+  production: "https://www.langeasy.ai/",
+  development: "https://langeasy-frontend-tk7ash3eaa-uc.a.run.app/",
+  QA: "https://langeasy-frontend-qa-tk7ash3eaa-uc.a.run.app/",
+};
+exports.frontEndUrl = frontEndUrl;

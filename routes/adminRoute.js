fb017963fd1954fa9dd52baa59bcf06248275055
AYOMIDE AJAYI <ayomideajayi007@gmail.com>
2024-05-30 12:01:14 +0100

    Merged Admin Controller

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -3,10 +3,12 @@ const protect = require("../middleWare/authMiddleware");
 
 const router = express.Router();
 
-const {searchUser, getTaskDetails } = require("../controllers/adminController");
+const {searchUser, getTaskDetails, getStats, getCompletedTasks } = require("../controllers/Admin/adminController");
 
 
 router.get("/searchuser", searchUser);
 router.get("/taskdetails", getTaskDetails);
+router.get("/dashboardstats" , getStats);
+router.get("/getcompletedtasks" , getCompletedTasks);
 
 module.exports = router; 
